e9b20a3790fb2b255f65ec0b18105f9541a3a7a1
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-25 15:04:17 -0400

    Created a useState variable to update to display the searched word in the component

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const handleChange = (event) => {
-    console.log(event);
+    setSearchTerm(event.target.value);
   };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+      <p>Searching for {searchTerm}.</p>
     </div>
   );
 };
