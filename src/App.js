fdcfbc790fb20a46ad722d3e110085e863d79a8e
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-25 13:21:02 -0400

    Moved the list array and function into its own component and shifted the functions into arrow functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
 import "./App.css";
+import { List } from "./List";
 
-const list = [
-  {
-    title: "React",
-    url: "https://react.js/org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function App() {
+export const App = () => {
   return (
     <div>
       <h1>My App</h1>
@@ -30,22 +12,7 @@ function App() {
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/List.js
@@ -0,0 +1,37 @@
+const listArray = [
+  {
+    title: "React",
+    url: "https://react.js/org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+export const List = () => {
+  return (
+    <ul>
+      {listArray.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
