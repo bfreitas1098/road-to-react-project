bd6707fb84992f0013188f64f501eafcb5cb6c17
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-25 13:23:24 -0400

    Moved the search field elements into their own Search component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./App.css";
 import { List } from "./List";
+import { Search } from "./Search";
 
 export const App = () => {
   return (
     <div>
       <h1>My App</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
+      <Search />
       <hr />
-
       <List />
     </div>
   );
---
+++ src/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+};
