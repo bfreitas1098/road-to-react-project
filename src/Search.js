62af6cf4632241b4ed24322761b5db03048f84f9
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-26 10:53:18 -0400

    Created a handleSubmit function to display the word being searched when it is submitted in the search form

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 
 export const Search = () => {
+  const [loaded, setLoaded] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleChange = (event) => {
+  let handleChange = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    setLoaded(true);
+    handleChange();
   };
 
   return (
     <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>Searching for {searchTerm}.</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={handleChange} />
+        <input type="submit" value="Search" />
+      </form>
+      <p>{loaded ? `Searching for ${searchTerm}` : `Enter a search word.`}</p>
     </div>
   );
 };
